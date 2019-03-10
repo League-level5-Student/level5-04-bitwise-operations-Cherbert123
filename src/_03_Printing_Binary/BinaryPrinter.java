b550b767fbcccd6aca15a8db5a3ebe60dab3398f
Your Name <you@example.com>
2019-03-10 12:07:50 -0700

    BinaryDecimal and Logic gates AND, OR XOR

--- src/_03_Printing_Binary/BinaryPrinter.java
+++ src/_03_Printing_Binary/BinaryPrinter.java
@@ -1,23 +1,35 @@
 package _03_Printing_Binary;
 
 public class BinaryPrinter {
-	//Complete the methods below so they print the passed in parameter in binary.
-	//Don't be afraid to use the methods that are already complete to finish the others.
-	//Create a main method to test your methods.
-	
-	public void printByteBinary(byte b) {
-		
+	// Complete the methods below so they print the passed in parameter in binary.
+	// Don't be afraid to use the methods that are already complete to finish the
+	// others.
+	// Create a main method to test your methods.
+
+	public static void main(String[] args) {
+		Byte b = 8;
+		printByteBinary(b);
+		Short s = 2;
+		printShortBinary(s);
+		int i = 32;
+		printIntBinary(i);
+		long l = 64;
+		printLongBinary(l);
 	}
-	
-	public void printShortBinary(short s) {
-		
+
+	public static void printByteBinary(byte b) {
+		System.out.println(Integer.toBinaryString(Integer.parseInt(Byte.toString(b))));
 	}
-	
-	public void printIntBinary(int i) {
-		
+
+	public static void printShortBinary(short s) {
+		System.out.println(Integer.toBinaryString(Integer.parseInt(Short.toString(s))));
 	}
-	
-	public void printLongBinary(long l) {
-		
+
+	public static void printIntBinary(int i) {
+		System.out.println(Integer.toBinaryString(i));
+	}
+
+	public static void printLongBinary(long l) {
+		System.out.println(Integer.toBinaryString(Integer.parseInt(Long.toString(l))));
 	}
 }
